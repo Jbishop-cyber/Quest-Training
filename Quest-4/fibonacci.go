f4b01e1f65f5aa8b21832261ea883de8d72a20f9
Jbishop <globalresearchinstitution@gmail.com>
2025-10-28 17:57:40 +0100

    Update fibonacci.go

--- Quest-4/fibonacci.go
+++ Quest-4/fibonacci.go
@@ -1,1 +1,14 @@
+package piscine
 
+func Fibonacci(index int) int {
+	if index < 0 {
+		return -1 // negative index returns -1
+	}
+	if index == 0 {
+		return 0 // base case: Fib(0) = 0
+	}
+	if index == 1 {
+		return 1 // base case: Fib(1) = 1
+	}
+	return Fibonacci(index-1) + Fibonacci(index-2) // recursive step
+}
